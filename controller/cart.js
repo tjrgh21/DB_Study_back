386603cfef8323cdab25df0d1b04c15298016541
tjrgh21 <xq46321@naver.com>
2025-02-06 16:18:33 +0900

    cart update

--- controller/cart.js
+++ controller/cart.js
@@ -6,7 +6,7 @@ exports.postCart = async(req, res) => {
         const { id } = req.body.id;
         const { date } = req.body.date;
         console.log(id)
-        const [ insertCart ] = await pool.query("INSERT INTO cart(user_id, created_date) VALUES(?,?)",[id, date])
+        const [ insertCart ] = await pool.query("INSERT INTO cart(created_date, user_id) VALUES(?,?)",[date, id])
         res.send({msg: "장바구니 생성" })
     }
     catch(err){
