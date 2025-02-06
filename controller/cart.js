8331919144e14e090b005e47509a7e1134006f04
MinGyu Lim <alsrb2277@naver.com>
2025-02-06 16:49:28 +0900

    feat: create cart

--- controller/cart.js
+++ controller/cart.js
@@ -3,11 +3,10 @@ const pool = require("../DB/db")
 
 exports.postCart = async(req, res) => {
     try{
-        const { id } = req.body.id;
-        const { date } = req.body.date;
+        const { id, pw, name } = req.body.user;
         console.log(id)
-        const [ insertCart ] = await pool.query("INSERT INTO cart(created_date, user_id) VALUES(?,?)",[date, id])
-        res.send({msg: "장바구니 생성" })
+        const [ insertCart ] = await pool.query("INSERT INTO cart(created_date, user_id) VALUES(NOW(),?)",[id])
+        res.send({msg2: "장바구니 생성" })
     }
     catch(err){
         console.error(err)
