944631c94280474d0d718cc7f29d6d295f6b8e56
MinGyu Lim <alsrb2277@naver.com>
2025-02-17 12:52:06 +0900

    feat: add delete function

--- controller/mypage.js
+++ controller/mypage.js
@@ -4,14 +4,14 @@ const pool = require("../DB/db")
 exports.getMyPage = async (req, res) => {
     try {
         const user = req.session.user; //세션에서 유저 가져오기
-        console.log("세션 정보:", req.session)
+        // console.log("세션 정보:", req.session)
         console.log("유저 정보:", user)
 
         const userInfo = await pool.query("SELECT id, name FROM user WHERE id = ?", [user]);
-        console.log("유저 정보 쿼리: ", userInfo);
+        // console.log("유저 정보 쿼리: ", userInfo);
         const card = await pool.query("SELECT * FROM credit WHERE user_id = ?", [user])
         const address = await pool.query("SELECT * FROM address WHERE user_id = ?", [user])
-        console.log("카드 쿼리: ", card, "주소 쿼리: ", address)
+        // console.log("카드 쿼리: ", card, "주소 쿼리: ", address)
 
         res.send({ user : userInfo[0][0], card: card[0], address: address[0] })
 
@@ -45,12 +45,13 @@ exports.postCredit = async (req, res) => {
 exports.deleteCard = async (req, res) => {
     const card_id = req.body.card_id; //삭제할 카드의 card_id
     const user = req.session.user
-    await pool.query("DELETE FROM credit WHERE card_id = ? AND user_id = ?", [card_id, user]);
+    await pool.query("DELETE FROM credit WHERE card_id = ? AND user_id = ?", [card_id.card_id, user]);
     res.send({ msg: "카드 목록에서 삭제" });
-    console.log("카드 삭제:", card_id, "번 째 카드")
+    console.log("카드 삭제:", card_id.card_id, "번 째 카드")
+    // console.log("유저 정보:", user)
 }
 
-//주소 추가
+//주소 추가 
 exports.postAddress = async (req, res) => {
     const { postal_code, address, detailed_address } = req.body.address
     console.log(`우편번호: ${postal_code}, 기본주소: ${address}, 상세주소: ${detailed_address}`)
@@ -65,9 +66,9 @@ exports.postAddress = async (req, res) => {
 exports.deleteAddress = async (req, res) => {
     const user = req.session.user
     const address_id = req.body.address_id; //삭제할 주소의 postal_code
-    await pool.query("DELETE FROM address WHERE address_id = ? AND user_id =?", [address_id, user]);
+    await pool.query("DELETE FROM address WHERE address_id = ? AND user_id =?", [address_id.address_id, user]);
     res.send({ msg: "주소 목록에서 삭제" });
-    console.log("주소 삭제: ", address_id, "번 째 주소")
+    console.log("주소 삭제: ", address_id.address_id, "번 째 주소")
 }
 
 // //주문내역
